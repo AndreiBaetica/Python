82a88d7447bfe85455a395e1990ed00243c2787f
Andrei Baetica <andrei.baetic@gmail.com>
2017-05-25 20:48:38 -0400

    Added decoder

---
+++ Decoder.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+url='http://warthunder.com/en/news/'
+r=requests.get(url)
+soup=BeautifulSoup(r.text,'html.parser')
+for heading in soup.find_all(class_="news-item__anons"):
+    if heading.a:
+        print(heading.a.text.replace("\n"," ").strip())
+    else:
+        print(heading.contents[0].strip())
