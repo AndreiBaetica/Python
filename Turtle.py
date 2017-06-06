6cd8a1ca6a4e0910175c58bd3e435afa291955e3
Andrei Baetica <andrei.baetic@gmail.com>
2017-06-05 22:37:24 -0400

    Fixed point class.

--- Turtle.py
+++ Turtle.py
@@ -1,21 +1,21 @@
-import turtle
-a=turtle.Turtle()
-n=8
-for i in range(n):
-    a.forward(70)
-    a.left(360/n)
-m=turtle.Turtle()
-m.shape("turtle")
-m.color("red")
-m.speed(3)
-m.penup()
-m.goto(-200,-50)
-m.pendown()
-delta=0.1*360/n
-for i in range(30):
-    m.forward(40+delta)
-    m.left(360/n+delta)
-    
+##import turtle
+##a=turtle.Turtle()
+##n=8
+##for i in range(n):
+##    a.forward(70)
+##    a.left(360/n)
+##m=turtle.Turtle()
+##m.shape("turtle")
+##m.color("red")
+##m.speed(3)
+##m.penup()
+##m.goto(-200,-50)
+##m.pendown()
+##delta=0.1*360/n
+##for i in range(30):
+##    m.forward(40+delta)
+##    m.left(360/n+delta)
+##    
 class point:
     def __init__(self,col,xcoord=0,ycoord=0,):
         self.colour=col
@@ -23,6 +23,6 @@ class point:
         self.y=ycoord
     def move(self,dx,dy):
         self.x=self.x+dx
-        selfy=self.y+dy
+        self.y=self.y+dy
 
 u=point.__init__('green', 4,8)
