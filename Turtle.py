e5e048ac98cacd11c5933509ec2d1b730b22edb1
Andrei Baetica <andrei.baetic@gmail.com>
2017-06-07 22:51:21 -0400

    Added __eq__, __repr__, and deck class.

--- Turtle.py
+++ Turtle.py
@@ -44,4 +44,17 @@ class card:
         return self.suit
     def setRank(self,rank):
         self.rank=rank
-    
+    def __repr__(self):
+        return 'Card('+str(self.rank)+','+str(self.suit)+')'
+    def __eq__(self,other):
+        if self.rank==other.rank and self.suit==other.suit:
+            return True
+        else:
+            return False
+
+class deck:
+    ranks={'2','3','4','5','6','7','8','9','10','J','Q','K','A'}
+    suits={'\u2660','\u2661','\u2662','\u2663'}
+    for rank in ranks:
+        for suit in suits:
+            print(rank,suit)
