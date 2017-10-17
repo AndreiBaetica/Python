b8491b9534e804c8525f819f3ba25e0cc27e3248
Andrei Baetica <andrei.baetic@gmail.com>
2017-10-16 23:58:00 -0400

    continued challenge questions

--- Python Challenge Questions 1.py
+++ Python Challenge Questions 1.py
@@ -142,3 +142,123 @@ def min_CAD_coins(price,payment):
     #The round function is called to eliminate python's float inacuracy.
     return (t,l,q,d,n)
 
+def min_enclosing_rectangle(radius,x,y):
+    '''
+    (number,number,number)->(number,number) or None
+    Gives lower left coordinate of a rectangle with minimal area that encompasses a circle defined by
+    radius, x, and y (origin coordinates).
+    Precondition: radius<0
+    '''
+    if radius<0:
+        return None
+    cxl=x-radius
+    cyb=y-radius
+    return(cxl,cyb)
+
+
+def series_sum():
+    '''
+    (none)->number
+    Prompts user for input n and returns 1000+sigma[1,n][1/n^2].
+    '''
+    n=int(input('Please enter a non-negative integer: '))
+    if n<0:
+        return None
+    else:
+        i=1
+        sum=1000
+        while i<n+1:
+            sum=sum+(1/(i**2))
+            i=i+1
+        return sum
+
+    
+def pell(n):
+    '''
+    (int)->int or None
+    Returns nth Pell number.
+    Precondition: n>=0
+    '''
+    if n<0:
+        return None
+    elif n==0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return(2*pell(n-1)+pell(n-2))
+
+    
+def countMembers(s):
+    '''
+    (str)->int
+    returns number of special characters in s.
+    '''
+    list=['e','f','g','h','i','j','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','2','3','4','5','6','!',',','\\']
+    tally=0
+    for i in s:
+        if i in list:
+            tally=tally+1
+    return tally
+
+
+def casual_number(s):
+    '''
+    (str)->int
+    Removes commas from numbers.
+    '''
+    try:
+        return int(s.replace(',',''))
+    except ValueError:
+        return None
+
+    
+def alienNumbers(s):
+    '''
+    (str)->int
+    Translates alien symbol sequences into numbers.
+    Preconditions: s is comprised only of alien symbols.
+    '''
+    list=[]
+    for i in s:
+        list.append(i)
+    for idx, i in enumerate(list):
+        if list[idx]=='T':
+            list[idx]=1024
+        if list[idx]=='y':
+            list[idx]=598
+        if list[idx]=='!':
+            list[idx]=121
+        if list[idx]=='a':
+            list[idx]=42
+        if list[idx]=='N':
+            list[idx]=6
+        if list[idx]=='U':
+            list[idx]=1
+    return sum(list)
+
+
+def alienNumbersAgain(s):
+    '''
+    (str)->int
+    Translates alien symbol sequences into numbers.
+    Preconditions: s is comprised only of alien symbols.
+    '''
+    #Code is the same as before because no string methods were used to begin with.
+    list=[]
+    for i in s:
+        list.append(i)
+    for idx, i in enumerate(list):
+        if list[idx]=='T':
+            list[idx]=1024
+        if list[idx]=='y':
+            list[idx]=598
+        if list[idx]=='!':
+            list[idx]=121
+        if list[idx]=='a':
+            list[idx]=42
+        if list[idx]=='N':
+            list[idx]=6
+        if list[idx]=='U':
+            list[idx]=1
+    return sum(list)
