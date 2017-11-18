efd8c17768ad37dc0a8171e9c454c61b3cd382ae
Andrei Baetica <andrei.baetic@gmail.com>
2017-11-18 14:00:00 +0100

    Added lab 1

---
+++ Labs/L1/L2P1.py
@@ -0,0 +1,74 @@
+import turtle 
+s=turtle.Screen() 
+t=turtle.Turtle() 
+
+# draw the smallest
+t.circle(10)
+
+# move and draw the next circle
+t.penup()
+t.goto(0, -40)
+t.pendown()
+t.circle(50)
+
+# move and draw the next circle
+t.penup()
+t.goto(0, -65)
+t.pendown()
+t.circle(75)
+
+# move and draw the next circle
+t.penup()
+t.goto(0, -90)
+t.pendown()
+t.circle(100)
+
+# move and draw the first line 
+t.penup()
+t.goto(0,10)
+t.pendown()
+t.forward(200)
+
+t.penup()
+t.goto(0,10)
+t.pendown()
+t.left(45)
+t.forward(200)
+
+t.penup()
+t.goto(0,10)
+t.pendown()
+t.left(45)
+t.forward(200)
+
+t.penup()
+t.goto(0,10)
+t.pendown()
+t.left(45)
+t.forward(200)
+
+t.penup()
+t.goto(0,10)
+t.pendown()
+t.left(45)
+t.forward(200)
+
+t.penup()
+t.goto(0,10)
+t.pendown()
+t.left(45)
+t.forward(200)
+
+t.penup()
+t.goto(0,10)
+t.pendown()
+t.left(45)
+t.forward(200)
+
+t.penup()
+t.goto(0,10)
+t.pendown()
+t.left(45)
+t.forward(200)
+
+
